115de9122023cba3114fada3f24a4f8bcdfa323f
jasonblu123 <126565272+jasonblu123@users.noreply.github.com>
2024-03-07 02:27:13 +0300

    Add files via upload

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+const Footer = () => {
+    return ( 
+        <footer>
+            <div className="footer__container">
+                <div className="fotter__copyright__container">
+                    <ul className='nav'>
+                        <li className="footer__copyright">©2023 Wear Shop Ltd.</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
