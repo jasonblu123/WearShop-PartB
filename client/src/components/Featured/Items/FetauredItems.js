87406103584c776999d539b59306245e2fd3fe29
jasonblu123 <126565272+jasonblu123@users.noreply.github.com>
2024-03-09 07:03:09 +0300

    Update FetauredItems.js

--- client/src/components/Featured/Items/FetauredItems.js
+++ client/src/components/Featured/Items/FetauredItems.js
@@ -7,7 +7,8 @@ import './FeaturedItems.css'
 const FeaturedItems = (props) => {
     return (
          
-        <div className="featured__products__container">
+        // <div className="featured__products__container">
+            <div>
             {/* <div className="featured__products">
                 <div className="featured__products__header">
                     <h3 className='featured__items__header__big'>Featured Items </h3><Link to="/shop" className='featured__header__small'>Show all<ArrowRightAltIcon /></Link>
